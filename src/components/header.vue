300bad7ec3dceec5e39ad5d96d1d2a4609640655
Роман <65034362+WEBSTER71@users.noreply.github.com>
2022-11-25 15:53:11 +0300

    header

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,6 +1,22 @@
 <template>
 	<header class="header">
-		header
+		<div class="header__container _container">
+			<div class="header__wrapper">
+				<div class="header__items">
+					<div class="header__title">Logo</div>
+				</div>
+				<div class="header__items">
+					<div class="header__button-box">
+						<button class="header__button btn">
+							<div class="btn-txt">Войти</div>
+						</button>
+						<button class="header__button btn">
+							<div class="btn-txt">Выйти</div>
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
 	</header>
 </template>
 
