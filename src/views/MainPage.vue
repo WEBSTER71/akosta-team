fbc7f21d39a328900282a239ad25156c58f86203
Роман <65034362+WEBSTER71@users.noreply.github.com>
2022-11-25 14:59:01 +0300

    akosta-team

---
+++ src/views/MainPage.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="wrapper">
+		<header-components />
+		<main class="page">
+			page
+		</main>
+		<footer-components />
+	</div>
+</template>
+
+<script>
+import headerComponents from '../components/header'
+import footerComponents from '../components/footer'
+
+export default {
+  name: 'mainPage',
+  components: {
+		headerComponents,
+		footerComponents,
+  }
+}
+</script>
